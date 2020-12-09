8a9e4a2182dbf085151dfe99f5960c9ab5cdd6bc
Tony <tr33947@gmail.com>
2020-12-09 20:01:49 +0300

    made changes

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 export SECRET_KEY='tony'
 export MAIL_USERNAME='EMAIL_USER'
 export MAIL_PASSWORD='EMAIL_PASS' 
-
+export DATABASE_URL=postgresql+psycopg2://moringa:Avamara34@localhost/pitch
 python3.9 manage.py server
 
 # export DATABASE_URL=postgresql+psycopg2://moringa:Avamara34@localhost/tony
